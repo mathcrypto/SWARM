9b374a2d85b197df488c527e1f3213d3f5e3d4fa
mathcrypto <bouguera_amira@yahoo.fr>
2017-05-03 15:27:10 +0200

    Create RecoverFile.js
    We send a receipt which has many informations (timestamp, blockNumber, ...and swarmHash). 
    This receipt is a result from create.js from proofOfExxistence repository.
    Now, using this receipt, we download the file again from his hash (URL).

---
+++ RecoverFile.js
@@ -0,0 +1,45 @@
+
+var express = require('express');
+var fs = require('fs');
+var router = express.Router();
+var multer = require('multer');
+var upload = multer({
+dest: 'uploads/'
+});
+var cryptojs = require("crypto-js");
+var sha256 = require("crypto-js/sha256");
+var Web3 = require('web3');
+var web3 = new Web3();
+web3.setProvider(new web3.providers.HttpProvider("http://192.168.1.104:8545"));
+var swarm = require("swarm-js").at("http://192.168.1.104:8500");
+router.post('/', upload.single('swarm'), function(req, res, next) {
+console.log("req body: \n" + JSON.stringify(req.body));
+console.log("req file: \n" + JSON.stringify(req.file));
+fs.readFile(req.file.path,'utf-8', function(err, content) {
+if (err) {
+res.status(500).send('ko');
+return;
+}
+
+
+
+  var filehash = content.substring(content.indexOf("SwarmHash") + "SwarmHash".length, content.indexOf("}"));
+  filehash = filehash.slice(4, 68).toString();
+
+console.log("filehash is" + filehash);
+
+
+swarm.download(filehash).then( function (buffer) {
+//console.log(buffer.toString());
+res.setHeader('Content-Disposition', 'attachment; filename=myFile.pdf');
+res.setHeader('Content-type', 'pdf');
+res.charset = 'utf-8';
+res.write(buffer);
+res.end();
+});
+
+});
+// Closes the Swarm process.
+
+});
+module.exports = router;
